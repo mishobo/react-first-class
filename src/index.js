712f65fd44a3f3fedf01df7b71c889b082a5b05e
mishobo <60912349+mishobo@users.noreply.github.com>
2022-07-11 14:34:22 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myFirstElement = <h1>Hello React!</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(myFirstElement)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const numbersOne = [1, 2, 3]
+const numbersTwo = [4, 5, 6]
+const numbersCombined = [...numbersOne, ...numbersTwo]
+
+const root1 = ReactDOM.createRoot(document.getElementById('root1'))
+root1.render(numbersCombined)
+
+function Car(props) {
+  return <h2>Hi, I am a {props.brand.model}</h2>
+}
+
+function Garage() {
+  const carInfo = { name: 'Mazda', model: 'Axela' }
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car brand={carInfo} />
+    </>
+  )
+}
+
+const root2 = ReactDOM.createRoot(document.getElementById('root2'))
+root2.render(<Garage />)
+
+function Football() {
+  const shoot = () => {
+    alert('Great Shot!')
+  }
+  return <button onClick={shoot}>Take the shot!</button>
+}
+
+const root3 = ReactDOM.createRoot(document.getElementById('root3'))
+root3.render(<Football />)
