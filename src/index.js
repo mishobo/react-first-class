6791e46053b407ebb8c44a78ba10f0c467e363c4
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-20 02:19:06 +0300

    React Hooks

--- src/index.js
+++ src/index.js
@@ -172,6 +172,29 @@ const TodoList = () => {
   )
 }
 
+// hook
+function FavoriteColor() {
+  const [color, setColor] = useState('red')
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button type="button" onClick={() => setColor('blue')}>
+        Blue
+      </button>
+      <button type="button" onClick={() => setColor('red')}>
+        Red
+      </button>
+      <button type="button" onClick={() => setColor('pink')}>
+        Pink
+      </button>
+      <button type="button" onClick={() => setColor('green')}>
+        Green
+      </button>
+    </>
+  )
+}
+
 function App() {
   return (
     <>
@@ -185,6 +208,7 @@ function App() {
       <Routing />
       <TodoList />
       <Car />
+      <FavoriteColor />
     </>
   )
 }
