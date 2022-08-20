bf8c1ee89850abc01c4add320e53296fd2c42a2a
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-20 19:07:02 +0300

    useFetch hook- fetching API data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
+import { useState, useEffect } from 'react'
+
+// Custom Hook// fetching data
 const Home = () => {
-  return <h1 style={{ color: 'red' }}>Home</h1>
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>Home</h1>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>
+        })}
+    </>
+  )
 }
 
 export default Home
